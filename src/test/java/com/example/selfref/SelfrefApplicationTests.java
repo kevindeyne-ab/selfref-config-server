4dcdddf47de62063d758018f2ec4f214cf2642a1
Kevin Deyne <kdeyne@accurate.com>
2025-08-11 14:09:36 -0700

    setup

---
+++ src/test/java/com/example/selfref/SelfrefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfrefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
